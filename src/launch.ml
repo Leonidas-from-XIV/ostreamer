1b67edf38b8edc9ccbd7008f6cc2e65759a6faac
Marek Kubica <marek@xivilization.net>
2012-12-29 02:59:17 +0100

    Added reading of file (with Batteries' help)

--- src/launch.ml
+++ src/launch.ml
@@ -1,8 +1,13 @@
+open Batteries_uni
+
 let _ =
     print_endline (Printf.sprintf "ost-launch %d" (Archive.version_number ()));
+    let f_in = File.open_in "test.gz" in
+    let content = IO.read_all f_in in
+    let l = String.length content in
     let handle = Archive.read_new () in
         ignore (Archive.read_support_filter_all handle);
         ignore (Archive.read_support_format_all handle);
-        ignore (Archive.read_open_memory handle "abc" 3);
+        ignore (Archive.read_open_memory handle content length);
         Archive.read_free handle;
     print_endline (Archive.version_string ())
