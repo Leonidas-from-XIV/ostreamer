d36d08a5c422200a441bd7f1d2226cb1ab35eaf4
Marek Kubica <marek@xivilization.net>
2012-12-30 02:19:10 +0100

    Fixed up the C minigunzip to decompress properly

--- demo/minigunzip.c
+++ demo/minigunzip.c
@@ -9,6 +9,7 @@ int main(void)
 
     archive_read_support_filter_all(a);
     archive_read_support_format_all(a);
+    archive_read_support_format_raw(a);
 
     FILE *fh = fopen("test.gz", "rb");
     fseek(fh, 0L, SEEK_END);
@@ -19,18 +20,23 @@ int main(void)
     int retval;
 
     retval = archive_read_open_memory(a, content, l);
-    printf("%d\n", retval);
+    //retval = archive_read_open_filename(a, "test.gz", 512);
+    //retval = archive_read_open_FILE(a, fh);
+    printf("read_open retval: %d\n", retval);
+    if (retval != ARCHIVE_OK) {
+        printf("retval error: %s\n", archive_error_string(a));
+    }
+
     archive_read_next_header(a, &e);
-    printf("%s\n", archive_entry_pathname(e));
+    printf("Pathname: %s\n", archive_entry_pathname(e));
 
+    const void* b;
+    size_t size;
+    int64_t offset;
+    retval = archive_read_data_block(a, &b, &size, &offset);
+    printf("read_data_block retval: %d\n", retval);
 
-    /*
-        ignore (Archive.read_open_memory handle content l);
-        ignore (Archive.read_next_header handle entry);
-        (* print_endline (Archive.entry_pathname entry); *)
-        ignore (Archive.read_data handle decompressed ldec);
-        print_endline decompressed;
-        Archive.read_free handle;
-    */
+    printf("buff: %s\n", b);
+
     return 0;
 }
