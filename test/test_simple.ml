5b584a80013f11f6c618092a28c2982be45c4a35
Marek Kubica <marek@xivilization.net>
2013-03-23 22:17:14 +0100

    Added another simple unit test, one that decompresses

--- test/test_simple.ml
+++ test/test_simple.ml
@@ -7,8 +7,27 @@ let test_version_getting _ =
         let name = String.sub version 0 10 in
         equ name "libarchive"
 
+let raw_gz_file =
+  "\x1f\x8b\x08\x00\xcd\x65\x42\x51\x00\x03\x0b\xc9\xc8\x2c\x56\x00\xa2\x44\
+  \x85\x92\xd4\xe2\x12\x85\xb4\xcc\x9c\x54\x2e\x8f\xd4\x9c\x9c\x7c\x2e\x00\
+  \xd2\xc2\x5e\x1b\x1a\x00\x00\x00"
+let raw_file = "This is a test file\nHello\n"
+
+let test_decompress_raw_single_file _ =
+    let handle = Archive.read_new_configured
+        [Archive.RawFormatReader]
+        [Archive.AllFilterReader] in
+    ignore (Archive.read_open_memory handle raw_gz_file (String.length raw_gz_file));
+    let archive_contents = Archive.extract_all handle in
+    let first = List.hd archive_contents in
+    match first with
+        | Archive.File (content, meta) -> equ content raw_file
+        | _ -> assert_failure "Did not get a single file"
+
+
 let suite = "Simple tests" >::: [
-        "test_version_getting" >:: test_version_getting
+        "test_version_getting" >:: test_version_getting;
+        "test_decompress_raw_single_file" >:: test_decompress_raw_single_file
         ]
 
 let _ =
