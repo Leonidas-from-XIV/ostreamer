4674aaaa49b769012532dae9977753234ef6bb13
Marek Kubica <marek@xivilization.net>
2013-03-28 18:34:03 +0100

    Reindent with ocp-indent

--- src/launch.ml
+++ src/launch.ml
@@ -4,8 +4,8 @@ open Archive
 
 (* helper for reduce *)
 let bang_fold acc = function
-    | "!" -> []::acc
-    | e -> (List.append (List.hd acc) [e])::(List.tl acc)
+  | "!" -> []::acc
+  | e -> (List.append (List.hd acc) [e])::(List.tl acc)
 
 (* splits a list of [a b ! c] into [[a b] [c] *)
 let bang_split pipe = List.rev (List.fold_left bang_fold [[]] pipe)
@@ -14,79 +14,79 @@ let bang_split pipe = List.rev (List.fold_left bang_fold [[]] pipe)
 let print_pipe pipe = List.print (fun out element -> IO.nwrite out element) stdout pipe
 
 let parse_commandline argv =
-    let largv = Array.to_list argv in
-    let args = List.tl largv in
-    bang_split args
+  let largv = Array.to_list argv in
+  let args = List.tl largv in
+  bang_split args
 
 let regfilep = function
-        | Unix.S_REG -> "Regular file"
-        | _ -> "Something nonregular"
+  | Unix.S_REG -> "Regular file"
+  | _ -> "Something nonregular"
 
 let filep = function
-        | Archive.File (content, metadata) -> "file"
-        | Archive.Directory metadata -> "directory"
+  | Archive.File (content, metadata) -> "file"
+  | Archive.Directory metadata -> "directory"
 
 let is_file = function
-        | Archive.File (_, _) -> true
-        | _ -> false
+  | Archive.File (_, _) -> true
+  | _ -> false
 
 let print_metadata metadata =
-        Printf.printf "File name: %s\n" metadata.Archive.pathname;
-        print_endline (regfilep metadata.Archive.filetype);
-        Option.print (fun out e -> IO.nwrite out (string_of_int e)) stdout metadata.Archive.size;
-        print_newline ();
-        Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.mtime;
-        print_newline ();
-        Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.atime;
-        print_newline ();
-        Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.ctime;
-        print_newline ();
-        Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.birthtime;
-        print_newline ();
-        Printf.printf "File uid: %d\n" metadata.Archive.uid;
-        Printf.printf "File gid: %d\n" metadata.Archive.gid;
-        Option.print (fun out e -> IO.nwrite out e) stdout metadata.Archive.uname;
-        print_newline ();
-        Option.print (fun out e -> IO.nwrite out e) stdout metadata.Archive.gname;
-        print_newline ()
+  Printf.printf "File name: %s\n" metadata.Archive.pathname;
+  print_endline (regfilep metadata.Archive.filetype);
+  Option.print (fun out e -> IO.nwrite out (string_of_int e)) stdout metadata.Archive.size;
+  print_newline ();
+  Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.mtime;
+  print_newline ();
+  Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.atime;
+  print_newline ();
+  Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.ctime;
+  print_newline ();
+  Option.print (fun out e -> IO.nwrite out (string_of_float e)) stdout metadata.Archive.birthtime;
+  print_newline ();
+  Printf.printf "File uid: %d\n" metadata.Archive.uid;
+  Printf.printf "File gid: %d\n" metadata.Archive.gid;
+  Option.print (fun out e -> IO.nwrite out e) stdout metadata.Archive.uname;
+  print_newline ();
+  Option.print (fun out e -> IO.nwrite out e) stdout metadata.Archive.gname;
+  print_newline ()
 
 let _ =
-    print_endline (Printf.sprintf "ost-launch %d" (Archive.version_number ()));
-    print_endline (Archive.version_string ());
-    let commands = parse_commandline Sys.argv in
-    let input_file = List.nth (List.nth commands 0) 0 in
-    let output_file = List.nth (List.nth commands 1) 0 in
-    let f_in = File.open_in input_file in
-    let content = IO.read_all f_in in
-    let l = String.length content in
-    let readhandle = Archive.read_new_configured
-        [Archive.AllFormatReader; Archive.RawFormatReader]
-        [Archive.AllFilterReader] in
-    let writehandle = Archive.write_new_configured Archive.RawFormatWriter
-        [Archive.GZipFilterWriter] in
-    let compressed = Archive.write_buffer_new () in
-    let written = Archive.written_ptr_new () in
-        Printf.printf "l: %d bytes\n" l;
-        let populated = Archive.feed_data readhandle content in
-        let archive_contents = Archive.extract_all populated in
-        match archive_contents with
-        | ErrorMonad.Success (entries) ->
-                List.print (fun out e -> IO.nwrite out (filep e)) stdout entries;
-                print_newline ();
-                let files = List.filter is_file entries in
-                let regular = List.hd files in
-                (match regular with
-                        | Archive.File (uncompressed, metadata) -> (
-                                print_metadata metadata;
-                                Printf.printf "uncompressed %s" uncompressed;
-                                (* write stuff *)
-                                Printf.printf "outused %d\n" (Archive.written_ptr_read written);
-                                ignore (Archive.write_open_memory writehandle compressed written);
-                                Archive.write_file writehandle (Archive.File (uncompressed, metadata));
-                                Printf.printf "outused %d\n" (Archive.written_ptr_read written);
-                                ignore (Archive.write_close writehandle);
-                                Printf.printf "outused %d\n" (Archive.written_ptr_read written);
-                                File.with_file_out output_file (fun f_out ->
-                                    IO.nwrite f_out (Archive.write_buffer_read compressed written)))
-                        | Archive.Directory metadata -> ())
-        | ErrorMonad.Failure (code,str) -> ()
+  print_endline (Printf.sprintf "ost-launch %d" (Archive.version_number ()));
+  print_endline (Archive.version_string ());
+  let commands = parse_commandline Sys.argv in
+  let input_file = List.nth (List.nth commands 0) 0 in
+  let output_file = List.nth (List.nth commands 1) 0 in
+  let f_in = File.open_in input_file in
+  let content = IO.read_all f_in in
+  let l = String.length content in
+  let readhandle = Archive.read_new_configured
+      [Archive.AllFormatReader; Archive.RawFormatReader]
+      [Archive.AllFilterReader] in
+  let writehandle = Archive.write_new_configured Archive.RawFormatWriter
+      [Archive.GZipFilterWriter] in
+  let compressed = Archive.write_buffer_new () in
+  let written = Archive.written_ptr_new () in
+  Printf.printf "l: %d bytes\n" l;
+  let populated = Archive.feed_data readhandle content in
+  let archive_contents = Archive.extract_all populated in
+  match archive_contents with
+  | ErrorMonad.Success (entries) ->
+    List.print (fun out e -> IO.nwrite out (filep e)) stdout entries;
+    print_newline ();
+    let files = List.filter is_file entries in
+    let regular = List.hd files in
+    (match regular with
+     | Archive.File (uncompressed, metadata) -> (
+         print_metadata metadata;
+         Printf.printf "uncompressed %s" uncompressed;
+         (* write stuff *)
+         Printf.printf "outused %d\n" (Archive.written_ptr_read written);
+         ignore (Archive.write_open_memory writehandle compressed written);
+         Archive.write_file writehandle (Archive.File (uncompressed, metadata));
+         Printf.printf "outused %d\n" (Archive.written_ptr_read written);
+         ignore (Archive.write_close writehandle);
+         Printf.printf "outused %d\n" (Archive.written_ptr_read written);
+         File.with_file_out output_file (fun f_out ->
+           IO.nwrite f_out (Archive.write_buffer_read compressed written)))
+     | Archive.Directory metadata -> ())
+  | ErrorMonad.Failure (code,str) -> ()
--- test/test_simple.ml
+++ test/test_simple.ml
@@ -5,58 +5,58 @@ open Archive
 let equ = assert_equal
 
 let test_version_getting _ =
-        let version = Archive.version_string () in
-        let name = String.sub version 0 10 in
-        equ name "libarchive"
+  let version = Archive.version_string () in
+  let name = String.sub version 0 10 in
+  equ name "libarchive"
 
 let raw_gz_file =
   "\x1f\x8b\x08\x00\xcd\x65\x42\x51\x00\x03\x0b\xc9\xc8\x2c\x56\x00\xa2\x44\
-  \x85\x92\xd4\xe2\x12\x85\xb4\xcc\x9c\x54\x2e\x8f\xd4\x9c\x9c\x7c\x2e\x00\
-  \xd2\xc2\x5e\x1b\x1a\x00\x00\x00"
+   \x85\x92\xd4\xe2\x12\x85\xb4\xcc\x9c\x54\x2e\x8f\xd4\x9c\x9c\x7c\x2e\x00\
+   \xd2\xc2\x5e\x1b\x1a\x00\x00\x00"
 let raw_file = "This is a test file\nHello\n"
 
 let test_decompress_raw_single_file _ =
-    let handle = Archive.read_new_configured
-        [Archive.RawFormatReader]
-        [Archive.AllFilterReader] in
-    let populated = Archive.feed_data handle raw_gz_file in
-    let archive_contents = Archive.extract_all populated in
-    match archive_contents with
-        | ErrorMonad.Success (xs) -> let first = List.hd xs in
-            (match first with
-            | Archive.File (content, meta) -> equ content raw_file
-            | _ -> assert_failure "Did not get a single file")
-        | ErrorMonad.Failure (code, str) -> assert_failure "Decompression failed"
+  let handle = Archive.read_new_configured
+      [Archive.RawFormatReader]
+      [Archive.AllFilterReader] in
+  let populated = Archive.feed_data handle raw_gz_file in
+  let archive_contents = Archive.extract_all populated in
+  match archive_contents with
+  | ErrorMonad.Success (xs) -> let first = List.hd xs in
+    (match first with
+     | Archive.File (content, meta) -> equ content raw_file
+     | _ -> assert_failure "Did not get a single file")
+  | ErrorMonad.Failure (code, str) -> assert_failure "Decompression failed"
 
 let test_nocompress_simple_file _ =
-    let handle = Archive.write_new_configured
-        Archive.RawFormatWriter [Archive.NoneFilterWriter] in
-    let openhandle = Archive.write_open_memory handle in
-    let meta = {
-        Archive.pathname = "foo";
-        filetype = Unix.S_REG;
-        atime = None;
-        birthtime = None;
-        ctime = None;
-        mtime = None;
-        gid = 1;
-        gname = None;
-        size = None;
-        uid = 1;
-        uname = None;
-        } in
-    let entry = Archive.File(raw_file, meta) in
-    let written = Archive.write_entry openhandle entry in
-    let res = Archive.write_close written in
-    match res with
-        | ErrorMonad.Success (res) -> equ res raw_file
-        | ErrorMonad.Failure (code, str) -> assert_failure "Compression failed"
+  let handle = Archive.write_new_configured
+      Archive.RawFormatWriter [Archive.NoneFilterWriter] in
+  let openhandle = Archive.write_open_memory handle in
+  let meta = {
+    Archive.pathname = "foo";
+    filetype = Unix.S_REG;
+    atime = None;
+    birthtime = None;
+    ctime = None;
+    mtime = None;
+    gid = 1;
+    gname = None;
+    size = None;
+    uid = 1;
+    uname = None;
+  } in
+  let entry = Archive.File(raw_file, meta) in
+  let written = Archive.write_entry openhandle entry in
+  let res = Archive.write_close written in
+  match res with
+  | ErrorMonad.Success (res) -> equ res raw_file
+  | ErrorMonad.Failure (code, str) -> assert_failure "Compression failed"
 
 let suite = "Simple tests" >::: [
-        "test_version_getting" >:: test_version_getting;
-        "test_decompress_raw_single_file" >:: test_decompress_raw_single_file;
-        "test_nocompress_simple_file" >:: test_nocompress_simple_file;
-        ]
+    "test_version_getting" >:: test_version_getting;
+    "test_decompress_raw_single_file" >:: test_decompress_raw_single_file;
+    "test_nocompress_simple_file" >:: test_nocompress_simple_file;
+  ]
 
 let _ =
-        run_test_tt_main suite
+  run_test_tt_main suite
